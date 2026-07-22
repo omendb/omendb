1f4dd9392285219467ba3f2be277a294400f24e2
Nick Russo <nijaru7@gmail.com>
2026-07-22 06:01:56 -0700

    test(storage): cover blob layouts in property model

--- tests/storage_property.rs
+++ tests/storage_property.rs
@@ -3,7 +3,7 @@
 #![allow(clippy::disallowed_methods)]
 
 use proptest::prelude::*;
-use seerdb::{BatchMutation, DB, Options};
+use seerdb::{BatchMutation, BlobStorageMode, DB, Options};
 use std::collections::BTreeMap;
 use tempfile::tempdir;
 
@@ -58,63 +58,88 @@ proptest! {
             1..48
         )
     ) {
-        let directory = tempdir().unwrap();
-        let path = directory.path().join("property.db");
-        let mut db = DB::create(&path, Options::default()).unwrap();
-        let mut model = Model::new();
-        let snapshot_boundary = operations.len().div_ceil(2);
-        let mut retained = None;
-
-        for (index, (key_id, value, is_put)) in operations.into_iter().enumerate() {
-            let key = key(key_id);
-            if is_put {
-                db.commit_batch(&[BatchMutation::Put {
-                    key: key.clone(),
-                    value: value.clone(),
-                }]).unwrap();
-                model.insert(key.clone(), value);
-            } else {
-                db.commit_batch(&[BatchMutation::Delete { key: key.clone() }])
-                    .unwrap();
-                model.remove(&key);
-            }
-
-            assert_eq!(db.get(&key).unwrap(), model.get(&key).cloned());
+        for segmented in [false, true] {
+            let directory = tempdir().unwrap();
+            let path = directory.path().join("property.db");
+            let options = Options {
+                blob_storage: if segmented {
+                    BlobStorageMode::Segmented
+                } else {
+                    BlobStorageMode::WholeImage
+                },
+                ..Options::default()
+            };
+            let mut db = DB::create(&path, options).unwrap();
+            let mut model = Model::new();
+            let snapshot_boundary = operations.len().div_ceil(2);
+            let blob_key = key(0);
+            let blob_value = vec![0xA5; 2_048];
+            db.commit_batch(&[BatchMutation::Put {
+                key: blob_key.clone(),
+                value: blob_value.clone(),
+            }])
+            .unwrap();
+            model.insert(blob_key, blob_value);
             assert_matches_model(&db, &model);
 
-            if index + 1 == snapshot_boundary {
-                let snapshot_id = db
-                    .retain_commit(db.durability_status().commit_id)
+            let initial_snapshot = db
+                .retain_commit(db.durability_status().commit_id)
+                .unwrap();
+            let mut retained = vec![(initial_snapshot, model.clone())];
+
+            for (index, (key_id, value, is_put)) in operations.iter().cloned().enumerate() {
+                let key = key(key_id);
+                if is_put {
+                    db.commit_batch(&[BatchMutation::Put {
+                        key: key.clone(),
+                        value: value.clone(),
+                    }])
                     .unwrap();
-                retained = Some((snapshot_id, model.clone()));
-            }
+                    model.insert(key.clone(), value);
+                } else {
+                    db.commit_batch(&[BatchMutation::Delete { key: key.clone() }])
+                        .unwrap();
+                    model.remove(&key);
+                }
 
-            if let Some((snapshot_id, snapshot_model)) = &retained {
-                assert_snapshot_matches_model(&db, *snapshot_id, snapshot_model);
-            }
-
-            if (index + 1) % 8 == 0 {
-                db.compact_with_limit(2).unwrap();
-                db.verify().unwrap();
-                db.close().unwrap();
-                db = DB::open(&path, Options::default()).unwrap();
-                db.verify().unwrap();
+                assert_eq!(db.get(&key).unwrap(), model.get(&key).cloned());
                 assert_matches_model(&db, &model);
-                if let Some((snapshot_id, snapshot_model)) = &retained {
+
+                if index + 1 == snapshot_boundary {
+                    let snapshot_id = db
+                        .retain_commit(db.durability_status().commit_id)
+                        .unwrap();
+                    retained.push((snapshot_id, model.clone()));
+                }
+
+                for (snapshot_id, snapshot_model) in &retained {
                     assert_snapshot_matches_model(&db, *snapshot_id, snapshot_model);
                 }
-            }
-        }
 
-        let (snapshot_id, _) = retained.take().unwrap();
-        db.release_snapshot(snapshot_id).unwrap();
-        db.compact_with_limit(2).unwrap();
-        db.vacuum().unwrap();
-        db.prune_history().unwrap();
-        db.verify().unwrap();
-        db.close().unwrap();
-        let mut reopened = DB::open(&path, Options::default()).unwrap();
-        reopened.verify().unwrap();
-        assert_matches_model(&reopened, &model);
+                if (index + 1) % 8 == 0 {
+                    db.compact_with_limit(2).unwrap();
+                    db.verify().unwrap();
+                    db.close().unwrap();
+                    db = DB::open(&path, Options::default()).unwrap();
+                    db.verify().unwrap();
+                    assert_matches_model(&db, &model);
+                    for (snapshot_id, snapshot_model) in &retained {
+                        assert_snapshot_matches_model(&db, *snapshot_id, snapshot_model);
+                    }
+                }
+            }
+
+            for (snapshot_id, _) in retained {
+                db.release_snapshot(snapshot_id).unwrap();
+            }
+            db.compact_with_limit(2).unwrap();
+            db.vacuum().unwrap();
+            db.prune_history().unwrap();
+            db.verify().unwrap();
+            db.close().unwrap();
+            let mut reopened = DB::open(&path, Options::default()).unwrap();
+            reopened.verify().unwrap();
+            assert_matches_model(&reopened, &model);
+        }
     }
 }
